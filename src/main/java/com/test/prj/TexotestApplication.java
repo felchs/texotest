031395c6b9a3f8338e1e69fc8cefba20d880a141
Felipe <felipe.chaves@hydrasystems.com.br>
2021-04-04 08:26:24 -0300

    basic project setup

---
+++ src/main/java/com/test/prj/TexotestApplication.java
@@ -0,0 +1,13 @@
+package com.test.prj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexotestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TexotestApplication.class, args);
+	}
+
+}
